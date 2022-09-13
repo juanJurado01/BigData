94eea1cd302a2afe32f028c4d3d6f2bef0709d8b
juanJurado01 <100302295+juanJurado01@users.noreply.github.com>
2022-09-13 01:51:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
+#Juan Jurado
 #importamos las librerias necesarias
 import json
-import urllib3
 import boto3
-#Juan Jurado
-#Creacion de la funcion que hace el scraping de la pagina del banco de la republica
-#y lo guarda en un archivo txt llamado dolar-timestamp.txt
+import datetime
+import csv 
+import pandas as pd
 
-def lambda_handler(event, context):
+def lambda_handler2():
+    #TODO implement
+    #Utilizamos la funcion datatime para agregarle al nombre del archivo la fecha de hoy
+    now2= datetime.datetime.now()
+    #con la funcion strftime convertimos el dato de la fecha en un string 
+    x2 = now2.strftime('%d %m %Y')
+    
     s3 = boto3.resource('s3')
-    http = urllib3.PoolManager()
-    #Declaramos la ruta de la pagina donde traeremos los archivos
-    url = 'https://totoro.banrep.gov.co/estadisticas-economicas/rest/consultaDatosService/consultaMercadoCambiario'
+    #Con download_file descargamos el archivo txt que se encuentra en el s3
+    s3.Bucket('dolarraw01').download_file('dolar '+ x2 + '.txt', '/tmp/dolar.txt')
+    
+    websites = pd.read_csv('dolar '+ x2 + '.txt',header = None) #leemos el archivo txt
+    websites.columns = ['FechaHora', 'Valor'] #Agregamos lo nombres de las columnas al csv
+    websites.to_csv('dolar_processed_ '+ x2 + '.csv', index = None) #generamos el archivo de texto como un archivo de tipo csv
+    
+    #Mediante la libreria boto3 le agregamos el bucket con la variable s3
+    client= boto3.client("s3","us-east-1")
+    s3= boto3.resource('s3')
+    bucket = s3.Bucket('dolarprocessed01')
 
-    resp = http.request('GET', url)
-    archivo = open("dolar-timestamp.txt", "w") #con el metodo open le damos el archivo que queremos abrir y con la w lo escribimos
-    archivo.write(print(resp.data.decode('utf -8')))
+    client.put_object(Body='dolar '+ x2 + '.txt', Bucket='dolarprocessed01', Key ='dolar_processed_ '+ x2 + '.csv')
 
-    archivo.close() #funcion para cerrar el archivo modificado
-    #se usa boto3 para subir el archivo a el bucket
-    data = open('dolar-timestamp.txt', 'rb')
-    s3.Bucket('dolarraw01').put_object(Key='dolar-timestamp.txt', Body=data)
+    #mensaje de retorno para validar que el codigo se ejecuto de manera satisfactoria
+    return{
+        'statusCode':200,
+        'body': json.dumps('Hola mundo!')
+    }
