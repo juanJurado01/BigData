bf2a42c2369f519b5a10489e7a7da52dc9c20e14
juanJurado01 <100302295+juanJurado01@users.noreply.github.com>
2022-09-13 09:05:16 -0500

    Update and rename functions.py to lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,11 @@
+#importamos las librerias necesarias
 import json
 import boto3
 import datetime
 from app import lambda_handler1
-from appcvs import lambda_handler2
+from appcsv import lambda_handler2
 
 #llamamos las dos funciones para que se ejecuten al mismo tiempo
-def lambda_handler(event, context):
-    #TODO implement
+def lambda_handler(event, contex):
     lambda_handler1()
     lambda_handler2()
