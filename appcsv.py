778891d95550617e565c8453a0121654a704c647
juanJurado01 <100302295+juanJurado01@users.noreply.github.com>
2022-09-13 14:50:14 -0500

    Update appcsv.py

--- appcsv.py
+++ appcsv.py
@@ -4,32 +4,29 @@ import json
 import boto3
 import datetime
 import csv 
-import pandas as pd
 
 def lambda_handler2():
     #TODO implement
-    #Utilizamos la funcion datatime para agregarle al nombre del archivo la fecha de hoy
+    # Se importa la funcion date time para sacar la fecha y poderla agregar al nombre del archivo txt
     now2= datetime.datetime.now()
-    #con la funcion strftime convertimos el dato de la fecha en un string 
+    #se llama la funcion strtime para convertir la funcion datetime a string 
     x2 = now2.strftime('%d %m %Y')
     
     s3 = boto3.resource('s3')
-    #Con download_file descargamos el archivo txt que se encuentra en el s3
     s3.Bucket('dolarraw12').download_file('dolar '+ x2 + '.txt', '/tmp/dolar.txt')
     
-    websites = pd.read_csv('dolar '+ x2 + '.txt',header = None) #leemos el archivo txt
-    websites.columns = ['FechaHora', 'Valor'] #Agregamos lo nombres de las columnas al csv
-    websites.to_csv('dolar_processed_ '+ x2 + '.csv', index = None) #generamos el archivo de texto como un archivo de tipo csv
+    #with open('dolar '+ x2 + '.txt','r',encoding = "utf-8") as a:
+    #    for line in a:
+    #        print(line)
     
-    #Mediante la libreria boto3 le agregamos el bucket con la variable s3
+    
     client= boto3.client("s3","us-east-1")
     s3= boto3.resource('s3')
     bucket = s3.Bucket('dolarprocessed12')
 
-    client.put_object(Body='dolar '+ x2 + '.txt', Bucket='dolarprocessed12', Key ='dolar_processed_ '+ x2 + '.csv')
+    client.put_object(Body='dolarraw12', Bucket='dolarprocessed12', Key ='dolar_processed_ '+ x2 + '.csv')
 
-    #mensaje de retorno para validar que el codigo se ejecuto de manera satisfactoria
     return{
         'statusCode':200,
-        'body': json.dumps('Hola mundo!')
+        'body': json.dumps('Hello from Lambda!')
     }
